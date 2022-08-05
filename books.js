b11ba6532712548d9b96f6aa18c1b32ec62c0751
Andres C <andrescn20@gmail.com>
2022-08-05 16:57:41 -0600

    Books Display Correctly After added

--- books.js
+++ books.js
@@ -11,29 +11,51 @@ const addBtn = document.getElementById("addBtn");
 
 const libraryContainer = document.getElementById("library");
 
-const bookContainer = document.createElement("div");
+let bookContainer = document.createElement("div");
 bookContainer.setAttribute("class", "bookContainer");
 
-const bookTitle = document.createTextNode("");
-// bookTitle.setAttribute("class", "bookTitle");
-
-const bookAuthor = document.createElement("p");
-bookAuthor.setAttribute("class", "bookAuthor");
-
-const bookPages = document.createElement("p");
-bookPages.setAttribute("class", "bookPages");
-
-const readStatus = document.createElement("p");
-readStatus.setAttribute("class", "bookStatus");
+let Silmarillion = {
+  title: "Silmarillion",
+  author: "Tolkien",
+  pages: "468",
+  readStatus: false,
+};
 
 let currentBook = {};
-let library = [""];
+let library = [Silmarillion];
 
 //Event Listener for button to Rock N Roll
 addBtn.addEventListener("click", (event) => {
   addToLibrary(generateBook());
-  //Displays Books:
-  library.foreach(generateBookDisplay());
+
+  console.log(library);
+
+  let bookTitle = document.createTextNode("");
+
+  let bookAuthor = document.createTextNode("");
+
+  let bookPages = document.createTextNode("");
+
+  let readStatus = document.createTextNode("");
+
+  library.forEach((element) => {
+    bookTitle.data = element.title;
+    bookAuthor.data = element.author;
+    bookPages.data = element.pages;
+
+    if (element.readStatus == false) {
+      readStatus.data = "Not Read";
+    } else {
+      readStatus.data = "Read";
+    }
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookPages);
+    bookContainer.appendChild(readStatus);
+
+    libraryContainer.appendChild(bookContainer);
+  });
 });
 
 // Retrieve Data and turns into array.
@@ -51,7 +73,10 @@ function generateBook() {
 
 //Object Constructor for making new Book entries
 function Book(title, author, pages) {
-  (this.title = title), (this.author = author), (this.page = pages);
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.readStatus = false);
 }
 
 //Receives the newly Generated Book and adds to Library Array.
@@ -59,8 +84,12 @@ function addToLibrary(currentBook) {
   library.push(currentBook);
 }
 
-function generateBookDisplay(currentBook) {
-  console.log(currentBook);
+function generateBookDisplay(item) {
+  console.log(item);
   bookContainer.appendChild(bookTitle);
   libraryContainer.appendChild(bookContainer);
 }
+
+// logFunction(){
+
+// }
